28401b22786aadf70edd6498f8a27bcbb88744cd
M.Uzair <uzair.iu17@gmail.com>
2020-07-17 18:18:55 +0500

    connected mongo db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 const mongoose = require('mongoose')
-const MONGODBURI = require('./keys')
+// const MONGODBURI = require('./keys')
 
 mongoose.connect("mongodb+srv://uzair:PWK6z6lbsofmWdnU@cluster0.mg7j7.mongodb.net/<dbname>?retryWrites=true&w=majority")
 mongoose.connection.on('connected',()=>{
