54a63876341b3d8816f023068b53671813db7ada
M.Uzair <uzair.iu17@gmail.com>
2020-07-17 18:17:33 +0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = 5000
+const mongoose = require('mongoose')
+const MONGODBURI = require('./keys')
 
+mongoose.connect("mongodb+srv://uzair:PWK6z6lbsofmWdnU@cluster0.mg7j7.mongodb.net/<dbname>?retryWrites=true&w=majority")
+mongoose.connection.on('connected',()=>{
+    console.log('connected to MOngo DB')
+})
+mongoose.connection.on('error',(err)=>{
+    console.log('error connecting',err )
+})
+   
 
 
 app.get('/',(req,res)=>{
---
+++ keys.js
@@ -0,0 +1,3 @@
+// module.exports   = {
+//     MONGODBURI  = "mongodb+srv://uzair:PWK6z6lbsofmWdnU@cluster0.mg7j7.mongodb.net/<dbname>?retryWrites=true&w=majority"
+// }
