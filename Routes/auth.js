c24e95f087b04fecd0a85ff9461014463035b06b
M.Uzair <uzair.iu17@gmail.com>
2020-07-20 12:59:32 +0500

    added jwt

--- Routes/auth.js
+++ Routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const JWT_SCERET = "efsgedtttvasolvb"
 
 router.get('/', (req, res) => {
     res.send('hello in auth')
@@ -52,7 +54,9 @@ router.post('/sign-in', (req, res) => {
             bcrypt.compare(password, savedUser.password)
                 .then(doMatch => {
                     if (doMatch) {
-                        res.json({ message: 'Login Succesfully' })
+                        const token = jwt.sign({ _id: savedUser._id }, JWT_SCERET)
+                        res.json({ token })
+                        // res.json({ message: 'Login Succesfully' })
                     } else {
                         return res.status(422).json({ eror: 'Invalid Email or Password' })
                     }
@@ -61,6 +65,6 @@ router.post('/sign-in', (req, res) => {
                 })
         })
 
-})  
+})
 
 module.exports = router
